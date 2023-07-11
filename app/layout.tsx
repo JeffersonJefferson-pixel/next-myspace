aa56310b0e426a91e3e6613741b614fd66ebc062
JeffersonJefferson-pixel <erenthewizard@gmail.com>
2023-07-11 22:02:55 +0700

    add navbar
    add auth api
    
    add blog api

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import AuthProvider from './AuthProvider'
+import NavMenu from './NavMenu'
 import './globals.css'
 import { Inter } from 'next/font/google'
 
@@ -14,8 +16,13 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <AuthProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <NavMenu />
+          {children}
+        </body>
+      </html>
+    </AuthProvider>
   )
 }
